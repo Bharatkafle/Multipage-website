4f370e4e09b5278d0ee712b71ae6b18aebee2d6b
unknown <bharatkafle775@gmail.com>
2022-04-09 17:22:44 +0545

    initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+//change navbar style on scroll
+window.addEventListener('scroll',() => {
+document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+});
+
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach( faq => {
+   faq.addEventListener('click',() =>{
+   	faq.classList.toggle('open');
+
+      const icon = faq.querySelector('.faq_icon i');
+      if(icon.className ==='fa-solid fa-plus'){
+          icon.className = "fa-solid fa-minus"
+       }else{
+         icon.className = "fa-solid fa-plus"
+       }
+
+
+   })
+})
+
+
+///show/hid nav menu
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closebtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+   menu.style.display = "flex";
+   closebtn.style.display = "inline-block"
+   menuBtn.style.display ="none"
+})
+///close nav menu
+const closeNav =() => {
+   menu.style.display ="none"
+   closebtn.style.display = "none";
+   menuBtn.style.display = "inline-block";
+}
+closebtn.addEventListener('click', closeNav);
